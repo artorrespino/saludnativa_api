9e052e4f289d6f05335cad2ca0ecd7d1acc6c8a7
Alexandra Torres <artorrespino@gmail.com>
2023-07-19 11:29:51 -0500

    Iniciando proyecto - configuración y estructura de carpetas

---
+++ src/main/java/com/saludnativa/SaludNativaApiApplication.java
@@ -0,0 +1,13 @@
+package com.saludnativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludNativaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaludNativaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saludnativa/SaludNativaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saludnativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludNativaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
